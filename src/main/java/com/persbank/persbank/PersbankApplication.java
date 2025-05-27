2a16e24444e2eed1ca276b10d5299f26108b629a
andrew.mintah <andrew.mintah@bigspark.dev>
2025-05-27 12:48:28 +0100

    imp DB connection correction

--- src/main/java/com/persbank/persbank/PersbankApplication.java
+++ src/main/java/com/persbank/persbank/PersbankApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(/*exclude = {DataSourceAutoConfiguration.class }*/
+		scanBasePackages = {"com.persbank.persbank.domain.model",
+				"com.persbank.persbank.domain.repository",
+				"com.persbank.persbank.domain.service",
+				"com.persbank.persbank.controller"})
 public class PersbankApplication {
 
 	public static void main(String[] args) {
